302d829806585385dfa9bd04c5fe22817adb5ada
Fadly safyuddin maulana <55474494+fadlysafyuddinmaulana@users.noreply.github.com>
2023-06-05 10:24:56 +0700

    Coba

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 import streamlit as st
+
+st.title('Hitung Luas Persegi')
+
+panjang = st.number_input ("Masukkan Nilai Panjang",0)
+lebar = st.number_input ("Masukkan Nilai Lebar",0)
+hitung = st.button ("Hitung Luas")
