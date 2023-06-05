65c5d134f0d51ecb4dea4392003eef2e6600b1f2
Fadly safyuddin maulana <55474494+fadlysafyuddinmaulana@users.noreply.github.com>
2023-06-05 14:30:54 +0700

    okayyyy

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
+# import streamlit as st
+
+# st.title('Hitung Luas Persegi')
+
+# panjang = st.number_input ("Masukkan Nilai Panjang",0)
+# lebar = st.number_input ("Masukkan Nilai Lebar",0)
+# hitung = st.button ("Hitung Luas")
+
+# if hitung :
+#     luas = panjang * lebar
+#     st.write("Luas Persegi Panjang Adalah = ", luas)
+#     st.success(f"Luas Persegi Panjang Adalah =  {luas}")kf
+
 import streamlit as st
+from streamlit_cropper import st_cropper
+from PIL import Image
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
-st.title('Hitung Luas Persegi')
+# Upload an image and set some options for demo purposes
+st.header("Cropper Demo")
+img_file = st.sidebar.file_uploader(label='Upload a file', type=['png', 'jpg'])
+realtime_update = st.sidebar.checkbox(label="Update in Real Time", value=True)
+box_color = st.sidebar.color_picker(label="Box Color", value='#0000FF')
+aspect_choice = st.sidebar.radio(label="Aspect Ratio", options=["1:1", "16:9", "4:3", "2:3", "Free"])
+aspect_dict = {
+    "1:1": (1, 1),
+    "16:9": (16, 9),
+    "4:3": (4, 3),
+    "2:3": (2, 3),
+    "Free": None
+}
+aspect_ratio = aspect_dict[aspect_choice]
 
-panjang = st.number_input ("Masukkan Nilai Panjang",0)
-lebar = st.number_input ("Masukkan Nilai Lebar",0)
-hitung = st.button ("Hitung Luas")
-
-if hitung :
-    luas = panjang * lebar
-    st.write("Luas Persegi Panjang Adalah = ", luas)
-    st.success(f"Luas Persegi Panjang Adalah =  {luas}")
+if img_file:
+    img = Image.open(img_file)
+    if not realtime_update:
+        st.write("Double click to save crop")
+    # Get a cropped image from the frontend
+    cropped_img = st_cropper(img, realtime_update=realtime_update, box_color=box_color,
+                                aspect_ratio=aspect_ratio)
+    
+    # Manipulate cropped image at will
+    st.write("Preview")
+    _ = cropped_img.thumbnail((150,150))
+    st.image(cropped_img)
